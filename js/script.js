a8d110d95423a7b63c3e28209cde086ae7a9d3c9
Serhii Haniuk <80706648+serhiihaniuk@users.noreply.github.com>
2021-03-20 18:51:39 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger'),
-    wideContent = document.querySelector('.pr-content__more'),
+    const  wideContent = document.querySelector('.pr-content__more'),
     mainContent = document.querySelector('.promo__content'), 
-    itemHide =  document.querySelector('.pr-content__items_hide'),
+    itemHide =  document.querySelector('.pr-content__items_hide');
     
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    });
 
     wideContent.addEventListener('click', () => {
         wideContent.classList.toggle('pr-content__more_active');

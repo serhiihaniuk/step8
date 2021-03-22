002b6be41807c9c80f42a9eb34820e90515bed75
Serhii Haniuk <80706648+serhiihaniuk@users.noreply.github.com>
2021-03-22 16:21:32 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const  wideContent = document.querySelector('.pr-content__more'),
+    mainContent = document.querySelector('.promo__content'), 
+    itemHide =  document.querySelector('.pr-content__items_hide'),
+    menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+    
+
+
+    wideContent.addEventListener('click', () => {
+        wideContent.classList.toggle('pr-content__more_active');
+        mainContent.classList.toggle('promo__content_active');
+        itemHide.classList.toggle('pr-content__items_hide');
+        
+    });
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    });
+
+    const swiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        slidesPerView: 1,
+        centeredSlides: true,
+        slidesOffsetBefore: 10,
+        loop: true,
+        spaceBetween: 50,
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+
+        flipEffect: {
+            slideShadows: false,
+          },
+
+          breakpoints: {
+            // when window width is >= 
+            576: {
+                slidesOffsetBefore: 150,
+            },
+          
+          }
+        
+      });
+      
+})

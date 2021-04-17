3a9afaed13ae5758f747569a84a4a9525546bf56
Serhii Haniuk <80706648+serhiihaniuk@users.noreply.github.com>
2021-04-17 16:53:14 +0200

    minor info changes

--- js/script.js
+++ js/script.js
@@ -1,95 +1,95 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const  wideContent = document.querySelector('.pr-content__more'),
-    mainContent = document.querySelector('.promo__content'), 
-    itemHide =  document.querySelector('.pr-content__items_hide'),
-    menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
-    
+window.addEventListener("DOMContentLoaded", () => {
+  const wideContent = document.querySelector(".pr-content__more"),
+    mainContent = document.querySelector(".promo__content"),
+    itemHide = document.querySelector(".pr-content__items_hide"),
+    menu = document.querySelector(".menu"),
+    menuItem = document.querySelectorAll(".menu_item"),
+    hamburger = document.querySelector(".hamburger"),
+    prItems = document.querySelectorAll(".hd");
 
+  let delay = 600;
 
-    wideContent.addEventListener('click', () => {
-        wideContent.classList.toggle('pr-content__more_active');
-        mainContent.classList.toggle('promo__content_active');
-        itemHide.classList.toggle('pr-content__items_hide');
-        
+  wideContent.addEventListener("click", () => {
+    wideContent.classList.toggle("pr-content__more_active");
+    mainContent.classList.toggle("promo__content_active");
+    prItems.forEach((item) => {
+      setTimeout(() => {
+        item.classList.toggle("dissapear");
+      }, delay);
+      delay += 150;
     });
+    delay = 500;
+  });
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("menu_active");
+  });
+
+  menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("menu_active");
     });
+  });
 
+  const swiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    slidesPerView: 1,
+    centeredSlides: true,
+    slidesOffsetBefore: 10,
+    loop: true,
+    spaceBetween: 50,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        });
-    });
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
 
-    const swiper = new Swiper('.swiper-container', {
-        // Optional parameters
-        slidesPerView: 1,
-        centeredSlides: true,
-        slidesOffsetBefore: 10,
-        loop: true,
-        spaceBetween: 50,
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
+    flipEffect: {
+      slideShadows: false,
+    },
 
-        flipEffect: {
-            slideShadows: false,
-          },
+    breakpoints: {
+      // when window width is >=
+      576: {
+        slidesOffsetBefore: 150,
+      },
+    },
+  });
 
-          breakpoints: {
-            // when window width is >= 
-            576: {
-                slidesOffsetBefore: 150,
-            },
-          
-          }
-        
-      });
+  new Swiper(".swiper-container-trending", {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 15,
+    loop: true,
+    parallax: false,
+    speed: 855,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".button-next",
+      prevEl: ".button-prev",
+    },
 
-      new Swiper('.swiper-container-trending', {
-        // Optional parameters
-        slidesPerView: 1,
-        spaceBetween: 15,
-        loop: true,
-        parallax: false,
-        speed: 855,
-        // Navigation arrows
-        navigation: {
-          nextEl: '.button-next',
-          prevEl: '.button-prev',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
 
-        flipEffect: {
-            slideShadows: false,
-          },
-          
-          breakpoints: {
-            // when window width is >= 
-            768: {
-              parallax: true,
-            },
-          
-          }
-        
-      });
-      
-})
+    flipEffect: {
+      slideShadows: false,
+    },
+
+    breakpoints: {
+      // when window width is >=
+      768: {
+        parallax: true,
+      },
+    },
+  });
+});
